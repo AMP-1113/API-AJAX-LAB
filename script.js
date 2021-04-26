36c3860991221211fdd75fd8aeb5c2d7654c617e
adam <paddenadam@gmail.com>
2021-04-26 17:39:23 -0400

    finished everything except second extended challenge

---
+++ script.js
@@ -0,0 +1,33 @@
+fetch("https://www.reddit.com/r/aww/.json")
+.then(response => response.json())
+.then(json => {
+
+    const getPost = json.data.children;
+    const getPost10 = getPost.slice(0, 10);
+    const container = document.getElementById("blogPost");
+    let counter = 0;
+        for (let child of getPost10) {
+            
+            const title = child.data.title;
+            const thumbnail = child.data.thumbnail;
+            const url = child.data.url_overridden_by_dest;
+
+            const pageTitleEl = document.createElement("h1")
+            pageTitleEl.classList.add("title");
+            pageTitleEl.innerText = title;
+            container.appendChild(pageTitleEl);
+
+            const thumbnailEl = document.createElement("img");
+            thumbnailEl.classList.add("img");
+            thumbnailEl.src = thumbnail;
+            container.appendChild(thumbnailEl);
+
+            const urlEl = document.createElement("a");
+            urlEl.classList.add("url");
+            urlEl.href = url;
+            urlEl.innerHTML = url
+            container.appendChild(urlEl);
+            counter += 1;
+        
+    }
+});
