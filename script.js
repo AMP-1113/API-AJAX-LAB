ab3eaac3ecfe91f17f1811b1e94b472df626bf6d
adam <paddenadam@gmail.com>
2021-04-26 20:23:16 -0400

    finished both extended challenges

--- script.js
+++ script.js
@@ -1,33 +1,123 @@
-fetch("https://www.reddit.com/r/aww/.json")
-.then(response => response.json())
-.then(json => {
+let aww = "https://www.reddit.com/r/aww/.json";
+let funny = "https://www.reddit.com/r/funny/.json";
+let memes = "https://www.reddit.com/r/memes/.json";
+const button = document.getElementById("go");
 
-    const getPost = json.data.children;
-    const getPost10 = getPost.slice(0, 10);
-    const container = document.getElementById("blogPost");
-    let counter = 0;
-        for (let child of getPost10) {
-            
-            const title = child.data.title;
-            const thumbnail = child.data.thumbnail;
-            const url = child.data.url_overridden_by_dest;
 
-            const pageTitleEl = document.createElement("h2")
-            pageTitleEl.classList.add("title");
-            pageTitleEl.innerText = title;
-            container.appendChild(pageTitleEl);
 
-            const thumbnailEl = document.createElement("img");
-            thumbnailEl.classList.add("img");
-            thumbnailEl.src = thumbnail;
-            container.appendChild(thumbnailEl);
 
-            const urlEl = document.createElement("a");
-            urlEl.classList.add("url");
-            urlEl.href = url;
-            urlEl.innerHTML = url
-            container.appendChild(urlEl);
-            counter += 1;
+button.addEventListener("click", event => {
+    event.preventDefault();
+    let inputValue = document.getElementById("inputNewSubreddit").value;
+  
+    if (inputValue === "aww") {
+            fetch(aww)
+    .then(response => response.json())
+    .then(json => {
+
+        const getPost = json.data.children;
+        const getPost10 = getPost.slice(0, 10);
+        const container = document.getElementById("blogPost");
         
+        let counter = 0;
+            for (let child of getPost10) {
+                
+                const title = child.data.title;
+                const thumbnail = child.data.thumbnail;
+                const url = child.data.url_overridden_by_dest;
+
+                const pageTitleEl = document.createElement("h2")
+                pageTitleEl.classList.add("title");
+                pageTitleEl.innerText = title;
+                container.appendChild(pageTitleEl);
+
+                const thumbnailEl = document.createElement("img");
+                thumbnailEl.classList.add("img");
+                thumbnailEl.src = thumbnail;
+                container.appendChild(thumbnailEl);
+
+                const urlEl = document.createElement("a");
+                urlEl.classList.add("url");
+                urlEl.href = url;
+                urlEl.innerHTML = url
+                container.appendChild(urlEl);
+                counter += 1;
+            
+            }
+
+        });
+    } else if (inputValue === "funny") {
+            fetch(funny)
+    .then(response => response.json())
+    .then(json => {
+        
+        const getPost = json.data.children;
+        const getPost10 = getPost.slice(0, 10);
+        const container = document.getElementById("blogPost");
+        
+        let counter = 0;
+            for (let child of getPost10) {
+                
+                const title = child.data.title;
+                const thumbnail = child.data.thumbnail;
+                const url = child.data.url_overridden_by_dest;
+
+                const pageTitleEl = document.createElement("h2")
+                pageTitleEl.classList.add("title");
+                pageTitleEl.innerText = title;
+                container.appendChild(pageTitleEl);
+
+                const thumbnailEl = document.createElement("img");
+                thumbnailEl.classList.add("img");
+                thumbnailEl.src = thumbnail;
+                container.appendChild(thumbnailEl);
+
+                const urlEl = document.createElement("a");
+                urlEl.classList.add("url");
+                urlEl.href = url;
+                urlEl.innerHTML = url
+                container.appendChild(urlEl);
+                counter += 1;
+            
+            }
+
+        });
+    } else if (inputValue === "memes") {
+        fetch(funny)
+    .then(response => response.json())
+    .then(json => {
+        
+        const getPost = json.data.children;
+        const getPost10 = getPost.slice(0, 10);
+        const container = document.getElementById("blogPost");
+        
+        let counter = 0;
+            for (let child of getPost10) {
+                
+                const title = child.data.title;
+                const thumbnail = child.data.thumbnail;
+                const url = child.data.url_overridden_by_dest;
+
+                const pageTitleEl = document.createElement("h2")
+                pageTitleEl.classList.add("title");
+                pageTitleEl.innerText = title;
+                container.appendChild(pageTitleEl);
+
+                const thumbnailEl = document.createElement("img");
+                thumbnailEl.classList.add("img");
+                thumbnailEl.src = thumbnail;
+                container.appendChild(thumbnailEl);
+
+                const urlEl = document.createElement("a");
+                urlEl.classList.add("url");
+                urlEl.href = url;
+                urlEl.innerHTML = url
+                container.appendChild(urlEl);
+                counter += 1;
+            
+            }
+        });
     }
+
 });
+
