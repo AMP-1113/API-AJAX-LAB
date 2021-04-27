25834b59fb70ebbb02dc8b1a0ffbecdfe14d17cf
adam <paddenadam@gmail.com>
2021-04-27 09:01:12 -0400

    consolidated some code but now the headers don't change correctly

--- script.js
+++ script.js
@@ -2,16 +2,8 @@ let aww = "https://www.reddit.com/r/aww/.json";
 let funny = "https://www.reddit.com/r/funny/.json";
 let memes = "https://www.reddit.com/r/memes/.json";
 const button = document.getElementById("go");
-
-
-
-
-button.addEventListener("click", event => {
-    event.preventDefault();
-    let inputValue = document.getElementById("inputNewSubreddit").value;
-  
-    if (inputValue === "aww") {
-            fetch(aww)
+const makeElement = () => {
+    fetch(aww)
     .then(response => response.json())
     .then(json => {
 
@@ -43,89 +35,32 @@ button.addEventListener("click", event => {
                 urlEl.classList.add("url");
                 urlEl.href = url;
                 urlEl.innerHTML = url
-                container.appendChild(urlEl);
-                
+                container.appendChild(urlEl); 
             
             }
 
         });
-    } else if (inputValue === "funny") {
-            fetch(funny)
-    .then(response => response.json())
-    .then(json => {
-        
-        const getPost = json.data.children;
-        const getPost10 = getPost.slice(0, 10);
-        const container = document.getElementById("blogPost");
+}
 
+button.addEventListener("click", event => {
+    event.preventDefault();
+    let inputValue = document.getElementById("inputNewSubreddit").value;
+  
+    if (inputValue === "aww") {
+        makeElement();
         const h2 = document.getElementById("h2");
-        
+        h2.innerText="Aww Subreddit"
+        h2.classList.add("header2");
+    } else if (inputValue === "funny") {
+        makeElement();
+        const h2 = document.getElementById("h2");
         h2.innerText="Funny Subreddit"
         h2.classList.add("header2");
-        
-            for (let child of getPost10) {
-                
-                const title = child.data.title;
-                const thumbnail = child.data.thumbnail;
-                const url = child.data.url_overridden_by_dest;
-
-                const pageTitleEl = document.createElement("h2")
-                pageTitleEl.classList.add("title");
-                pageTitleEl.innerText = title;
-                container.appendChild(pageTitleEl);
-
-                const thumbnailEl = document.createElement("img");
-                thumbnailEl.classList.add("img");
-                thumbnailEl.src = thumbnail;
-                container.appendChild(thumbnailEl);
-
-                const urlEl = document.createElement("a");
-                urlEl.classList.add("url");
-                urlEl.href = url;
-                urlEl.innerHTML = url
-                container.appendChild(urlEl);
-            
-            }
-
-        });
     } else if (inputValue === "memes") {
-        fetch(funny)
-    .then(response => response.json())
-    .then(json => {
-        
-        const getPost = json.data.children;
-        const getPost10 = getPost.slice(0, 10);
-        const container = document.getElementById("blogPost");
-
+        makeElement();
         const h2 = document.getElementById("h2");
-        
         h2.innerText="Memes Subreddit"
         h2.classList.add("header2");
-        
-            for (let child of getPost10) {
-                
-                const title = child.data.title;
-                const thumbnail = child.data.thumbnail;
-                const url = child.data.url_overridden_by_dest;
-
-                const pageTitleEl = document.createElement("h2")
-                pageTitleEl.classList.add("title");
-                pageTitleEl.innerText = title;
-                container.appendChild(pageTitleEl);
-
-                const thumbnailEl = document.createElement("img");
-                thumbnailEl.classList.add("img");
-                thumbnailEl.src = thumbnail;
-                container.appendChild(thumbnailEl);
-
-                const urlEl = document.createElement("a");
-                urlEl.classList.add("url");
-                urlEl.href = url;
-                urlEl.innerHTML = url
-                container.appendChild(urlEl);
-            
-            }
-        });
     }
 
 });
